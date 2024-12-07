57a3f03a30f933faeb9642a5e8ec89696f3ab6f3
dwi setyabudi <dwisetbu@gmail.com>
2024-12-07 23:50:09 +0700

    router dom

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -4,6 +4,7 @@ import Dashboard from "../../Pages/Content/Dashboard";
 import Slangword from "../../Pages/Content/KamusKata/Slangword";
 import Stopword from "../../Pages/Content/KamusKata/Stopword";
 import Dataset from "../../Pages/Content/Dataset";
+import Preprocessing from "../../Pages/Content/Preprocesing/Preprocesing";
 
 const Content = () => {
   return (
@@ -17,7 +18,7 @@ const Content = () => {
         <Route path="/kamuskata/stopword" element={<Stopword />} />
         {/* Other Routes */}
         <Route path="/dataset" element={<Dataset />} />
-        <Route path="/contact" element={<h2>Contact Us</h2>} />
+        <Route path="/contact" element={<Preprocessing />} />
       </Routes>
     </div>
   );
