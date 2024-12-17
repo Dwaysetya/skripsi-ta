f1b58655b8aeb84cc9b87487c51641672b52424b
dwi setyabudi <dwisetbu@gmail.com>
2024-12-17 21:58:05 +0700

    signup update

--- src/Pages/Login/Sign-Up/index.js
+++ src/Pages/Login/Sign-Up/index.js
@@ -1,67 +1,81 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input, Flex } from "antd";
+import Label from "../../../Components/Elements/Label";
 const SignUp = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
   return (
-    <Form
-      name="signup"
-      initialValues={{
-        remember: true,
-      }}
-      style={{
-        maxWidth: 360,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="Create Password"
+    <div>
+      <div style={{}}>
+        <Label
+          htmlFor="Sign-In"
+          text="Sign-In"
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            fontSize: "20px",
+          }}
         />
-        <Input
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="Create Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Flex justify="space-between" align="center">
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox style={{ color: "white" }}>Remember me</Checkbox>
-          </Form.Item>
-        </Flex>
-      </Form.Item>
+      </div>
+      <Form
+        name="signup"
+        initialValues={{
+          remember: true,
+        }}
+        style={{
+          maxWidth: 360,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Create Password"
+          />
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Create Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox style={{ color: "white" }}>Remember me</Checkbox>
+            </Form.Item>
+          </Flex>
+        </Form.Item>
 
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          Log in
-        </Button>
-        or <a href="">Register now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+          or <a href="">Register now!</a>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 export default SignUp;
