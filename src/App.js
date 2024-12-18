e514022984e23be02b5121ee302dfc772b854c0e
dwi setyabudi <dwisetbu@gmail.com>
2024-12-18 23:24:50 +0700

    app routes update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import React, { Component } from "react";
-import Slangword from "./Pages/Content/KamusKata/Slangword";
-import Dashboard from "./Pages/Content/Dashboard";
 import Sidebar from "./Pages/Sidebar";
-import LayoutPages from "./Pages/Layout";
 import Layout from "antd/es/layout/layout";
 import IndexHeaders from "./Pages/Header";
 import Content from "./Components/Content";
-import Foter from "./Pages/Footer";
-import SignIn from "./Pages/Login/Sign-In";
-import Login from "./Pages/Login";
-import LoginRoute from "./Components/LoginRoute";
-import Home from "./Pages/Home";
-import Register from "./Pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" Component={Home} />
-          <Route path="/login" Component={Register} />
-        </Routes>
-      </Router>
-      {/* <Router>
         <Layout>
           <IndexHeaders />
           <Layout>
@@ -39,7 +19,7 @@ function App() {
             </Layout>
           </Layout>
         </Layout>
-      </Router> */}
+      </Router>
     </div>
   );
 }
